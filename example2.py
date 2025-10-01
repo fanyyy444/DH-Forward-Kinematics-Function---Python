735f43985b732f73b13cdda10d33c0880b7b3f4c
Estefany Hernández <hlfany4@gmail.com>
2025-10-01 12:24:46 -0600

    Archivos modificados

--- example2.py
+++ example2.py
@@ -17,7 +17,7 @@ print("\nSymbolic example:")
 q1, q2 = sp.symbols('q1 q2')
 l1, l2 = sp.symbols('l1 l2')
 dh_params_sym = [
-    [q1, 0, l1, 0],
+    [q1, 0, 0, 0],
     [q2, 0, l2, 0],
 ]
 H_sym_class = ForwardKinematicsDH.symbolic(dh_params_sym)
