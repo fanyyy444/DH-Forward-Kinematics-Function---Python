a2a14621762886cd814369896a4b3c16a13cc428
Estefany Hernández <hlfany4@gmail.com>
2025-10-01 19:41:48 -0600

    Archivo modificado

--- Robot RRR.py
+++ Robot RRR.py
@@ -18,7 +18,7 @@ def limpiar_ceros(matriz, umbral=1e-10):
     return matriz_limpia
 
 
-# Numeric example: 2-joint planar robot (all revolute, a1=1, a2=1)
+# Numeric example: 3-joint planar robot (all revolute, a1=1, a2=1)
 print("Numeric example:")
 dh_params = [
     [np.pi/4, 0, 1, 0],
@@ -33,12 +33,12 @@ print("End-effector transformation matrix:")
 print(H_class_limpia)
 
 
-# symbolic example: 2-joint planar robot 
+# symbolic example: 3-joint planar robot 
 print("\nSymbolic example:")
-q1, q2, q3= sp.symbols('q1 q2 q3')
-l1, l2, l3 = sp.symbols('l1 l2 l3')
+q1, q2, q3= sp.symbols('q1 q2 q3') #se agrego el simbolo para q3
+l1, l2, l3 = sp.symbols('l1 l2 l3')  #se agrego el simbolo para l3
 dh_params_sym = [
-    [q1, 0, l1, sp.pi/2],   # se hizo el cambio de las variables para que coincidan con los valores de la matriz para un robot RR
+    [q1, 0, l1, sp.pi/2],   # se hizo el cambio de las variables para que coincidan con los valores de la matriz para un robot RRR
     [q2, 0, l2, 0],   # se cambiaron tanto los ángulos q1, q2, las distancias a se quedaron en 0, la distancia l se cambio tanto l1 y l2 y el angulo alpha se agino el valor de pi/2 y 0 para los demas casos
     [q3, 0, l3, 0],   #se agrega la siguiente linea para la articulacion 3 
 ]
